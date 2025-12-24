8f0d445e4937cebc398d165d51f81690b36916de
davinoroes <davinoroes1918@gmail.com>
2025-12-24 00:23:53 -0300

    criação da view de registrar usuário

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import login,logout
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+
+def register_view(request):
+    if request.method == 'POST':
+        user_form = UserCreationForm(request.POST)
+        if user_form.is_valid():
+            user_form.save()
+    else:
+        UserCreationForm()
+    return render(request,'register.html', {'user_form' : user_form})
+
+
+
+
