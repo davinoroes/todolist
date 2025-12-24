67a6f4087dea893596cb5a479858f4188385fbf7
davinoroes <davinoroes1918@gmail.com>
2025-12-24 01:05:11 -0300

    registro do model task na admin page

--- todolist/admin.py
+++ todolist/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from todolist.models import TaskModel
 
 # Register your models here.
+
+class TaskAdminModel(admin.ModelAdmin):
+    list_display = ('tittle','active','user',)
+
+
+admin.site.register(TaskModel,TaskAdminModel)
