c7d55c212bc40797c691d890c480f1ca35ec22fb
davinoroes <davinoroes1918@gmail.com>
2025-12-24 00:42:04 -0300

    Criação do model da task

--- todolist/models.py
+++ todolist/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class TaskModel(models.Model):
+    tittle = models.CharField(max_length=40, blank=True,null=True)
+    active = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE,blank=True,null=True)
+    
+
+
+
+
