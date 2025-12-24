aec9d35c9c2f2c6a83aa6f9d93b3581cb22f16df
davinoroes <davinoroes1918@gmail.com>
2025-12-24 12:20:12 -0300

    Adição da url de login e de um metodo base dentro do model task

--- core/urls.py
+++ core/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from accounts.views import register_view
+from accounts.views import register_view,login_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('register/', register_view, name='register')
+    path('register/', register_view, name='register'),
+    path('login/', login_view, name='login'),
 ]
--- todolist/models.py
+++ todolist/models.py
@@ -7,6 +7,9 @@ class TaskModel(models.Model):
     tittle = models.CharField(max_length=40, blank=True,null=True)
     active = models.BooleanField(default=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE,blank=True,null=True)
+
+    def __str__(self):
+        return self.tittle
     
 
 
