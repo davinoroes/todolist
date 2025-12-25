3745dd1b096e9fd9d26946430f8963bcd8c408c7
davinoroes <davinoroes1918@gmail.com>
2025-12-25 13:30:35 -0300

    Adição de função de busca de task dentro da view

--- todolist/views.py
+++ todolist/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from todolist.models import TaskModel
 
 # Create your views here.
+
+class TasksView(ListView):
+    model = TaskModel
+    template_name = 'tasks.html'
+    context_object_name = 'tasks'
+
+    def get_queryset(self):
+        tasks = super().get_queryset().order_by('tittle')
+        search = self.request.GET.get('search')
+        if search:
+            tasks = tasks.filter(tittle__icontains=search)
+        return tasks
+
+    
+    
+
