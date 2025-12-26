f1da2d1906ef11375c3abdf2f7d2af6168a69b34
davinoroes <davinoroes1918@gmail.com>
2025-12-26 00:03:23 -0300

    Mudança na taskview para filtro de tarefas de acordo com usuário

--- todolist/views.py
+++ todolist/views.py
@@ -1,20 +1,21 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from todolist.models import TaskModel
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 
-class TasksView(ListView):
+class TasksView(LoginRequiredMixin,ListView):
     model = TaskModel
     template_name = 'tasks.html'
     context_object_name = 'tasks'
 
     def get_queryset(self):
-        tasks = super().get_queryset().order_by('tittle')
+        tasks = TaskModel.objects.filter(user=self.request.user)
         search = self.request.GET.get('search')
         if search:
             tasks = tasks.filter(tittle__icontains=search)
-        return tasks
+        return tasks.order_by('tittle')
 
     
     
